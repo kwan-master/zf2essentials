56cdabb1719bc6ebb2d426d03d4f2eecd23c5267
Tiago Mestre <tiago.mestre@log.pt>
2015-03-17 20:26:16 +0000

    mais validadores

--- module/Market/src/Market/Form/PostFormFilter.php
+++ module/Market/src/Market/Form/PostFormFilter.php
@@ -2,8 +2,11 @@
 
 namespace Market\Form;
 
+
 use Zend\InputFilter\Input;
 use Zend\InputFilter\InputFilter;
+use Zend\Validator\Regex;
+use Zend\I18n\Validator\Alnum;
 
 class PostFormFilter extends InputFilter{
 
@@ -40,17 +43,38 @@ class PostFormFilter extends InputFilter{
         $description->getValidatorChain()
             ->attachByName("StringLength",array("min"=> 1, "max"=> 120));
 
-        $date_expires = new Input("description");
+        $name_regex = new Regex(array("pattern"=> '/^[a-zA-Z]*$/'));
+        $name_regex->setMessage("Contact name should only contain letters for a to z");
+        $contact_name = new Input("contact_name");
+
+        $contact_name->getValidatorChain()
+            ->attach($name_regex);
+
+        $phone_regex = new Regex(array("pattern"=> '/^[\d-]+$/'));
+        $contact_phone = new Input("contact_phone");
+
+        $contact_phone->getValidatorChain()
+                      ->attach($phone_regex);
+
+
 
 
         $email = new Input("contact_email");
         $email->getValidatorChain()->attachByName("EmailAddress");
+
+        $validator = new Alnum(array('allowWhiteSpace' => true));
+        $delete_code = new Input("delete_code");
+        $delete_code->getValidatorChain()
+                    ->attach($validator);
 
 
 
         $this->add($category)
             ->add($title)
             ->add($description)
+            ->add($contact_name)
+            ->add($contact_phone)
+            ->add($delete_code)
             ->add($email);
 
     }
